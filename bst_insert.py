ab95bd76c202c7a0c0aeb9d5f14f9dc0f08f27d4
Arkaprava Chakraborty <arkapravac366@gmail.com>
2026-02-23 11:29:52 +0530

    Refactor BST insertion and level order traversal

--- bst_insert.py
+++ bst_insert.py
@@ -1,55 +1,63 @@
 from collections import deque
+
 class Node:
-    def __init__(self,val):
+    def __init__(self, val):
         self.data = val
         self.left = None
         self.right = None
 
-    def getHeight(root, h):
-        if root is None:
-            return h-1
-            return max(getHeight(root.left,h+1), getHeight(root.right,h+1))
+def getHeight(root):
+    if root is None:
+        return -1
+    return 1 + max(getHeight(root.left), getHeight(root.right))
 
-    def levelOrder(root):
-        queue = deque()
-        queue.append((root,0))
-
-        lastLevel = 0
-        height = getHeight(root,0)
-
-        while queue:
-            node, lvl=queue.popleft()
-
-            if lvl > lastLevel:
-                print()
-                lastLevel = lvl
-
-            if lvl > height:
-                break
-
-            print("N" if node.data == -1 else node.data, end=" ")
-
-            if node.data == -1:
-                continue
-
-            if node.left is None:
-                queue.append((Node(-1)), lvl + 1)
-            else:
-                queue.append((node.left, lvl + 1))
+def levelOrder(root):
+    if not root:
+        return
+    
+    queue = deque([(root, 0)])
+    height = getHeight(root)
+    lastLevel = 0
+    
+    while queue:
+        node, lvl = queue.popleft()
+        
+        if lvl > lastLevel:
+            print()
+            lastLevel = lvl
             
-            if node.right is None:
+        if lvl > height:
+            break
+            
+        print("N" if node.data == -1 else node.data, end=" ")
+        
+        if node.data == -1:
+            if lvl < height:
                 queue.append((Node(-1), lvl + 1))
-            else:
-                queue.appen((node.right, lvl + 1))
+                queue.append((Node(-1), lvl + 1))
+            continue
 
-    def inser(root,key):
-        if root is None:
-            return Node(key)
+        if node.left:
+            queue.append((node.left, lvl + 1))
+        elif lvl < height:
+            queue.append((Node(-1), lvl + 1))
+            
+        if node.right:
+            queue.append((node.right, lvl + 1))
+        elif lvl < height:
+            queue.append((Node(-1), lvl + 1))
 
-        if(key < root.data):
-            root.left = insert(root.left, key)
-        else:
-            root.right = insert(root.right, key)
-        
-        return root
-        
+def insert(root, key):
+    if root is None:
+        return Node(key)
+    if key < root.data:
+        root.left = insert(root.left, key)
+    elif key > root.data:
+        root.right = insert(root.right, key)
+    return root
+
+root = None
+for val in [22, 12, 30, 8, 20, 30, 15]:
+    root = insert(root, val)
+
+levelOrder(root)
