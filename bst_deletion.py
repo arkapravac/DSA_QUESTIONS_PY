f52d348d2f297c599e7f23de94fd2a30b5db0a31
Arkaprava Chakraborty <arkapravac366@gmail.com>
2026-02-23 11:48:03 +0530

    Update bst_deletion.py

--- bst_deletion.py
+++ bst_deletion.py
@@ -1,57 +1,84 @@
 from collections import deque
 
 class Node:
-    def __init__(self,data):
+    def __init__(self, data):
         self.data = data
         self.left = None
-        self.right =  None
+        self.right = None
 
-    def dele_node(root, key):
-        if not root:
+def remove_deepest(root, d_node):
+    q = deque([root])
+    while q:
+        temp = q.popleft()
+        if temp is d_node:
             return None
-
-        q = deque([root])
-        node = None
-        last = None
-
-        while q:
-            last = q.popleft()
-            if last.data == key:
-                node = last
-            if last.left:
-                q.append(last.left)
-            if last.right:
-                q.append(last.right)
-
-        if node:
-            node.data = last.fata
-            remove_deepest(root, last)
-
-        return root
-
-    def remove_deeoest(root, d):
-        q = deque([root])
-        while q:
-            temp = q.popleft()
-            if temp.left == d:
+        if temp.right:
+            if temp.right is d_node:
                 temp.right = None
                 return
-            if temp.right == d:
-                temp.right = None
-                return
-            if temp.left:
-                q.appenf(temp.left)
-            if temp.right:
+            else:
                 q.append(temp.right)
+        if temp.left:
+            if temp.left is d_node:
+                temp.left = None
+                return
+            else:
+                q.append(temp.left)
 
-    def level_order(root):
-        if not root:
-            return
-        q = deque([root])
-        while q:
-            node = q.popleft()
-            print(node.data,end=" ")
-            if node.left:
-                q.append(node.left)
-            if node.right:
-                q.append(node.right)
+def delete_node(root, key):
+    if not root:
+        return None
+    if not root.left and not root.right:
+        return None if root.data == key else root
+        
+    q = deque([root])
+    key_node = None
+    last_node = None
+    while q:
+        last_node = q.popleft()
+        if last_node.data == key:
+            key_node = last_node
+        if last_node.left:
+            q.append(last_node.left)
+        if last_node.right:
+            q.append(last_node.right)
+            
+    if key_node:
+        deepest_val = last_node.data
+        remove_deepest(root, last_node)
+        key_node.data = deepest_val
+    return root
+
+def level_order(root):
+    if not root:
+        return
+    q = deque([root])
+    while q:
+        node = q.popleft()
+        print(node.data, end=" ")
+        if node.left:
+            q.append(node.left)
+        if node.right:
+            q.append(node.right)
+
+def insert(root, key):
+    if root is None:
+        return Node(key)
+    if key < root.data:
+        root.left = insert(root.left, key)
+    elif key > root.data:
+        root.right = insert(root.right, key)
+    return root
+
+root = None
+for val in [22, 12, 30, 8, 20, 30, 15]:
+    root = insert(root, val)
+
+print("Before:", end=" ")
+level_order(root)
+
+x = int(input("\nEnter element to delete: "))
+root = delete_node(root, x)
+
+print("After:", end=" ")
+level_order(root)
