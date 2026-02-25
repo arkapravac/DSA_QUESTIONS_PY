c25db37d024fa8850288bf9146da0d93ab1e85a1
Arkaprava Chakraborty <arkapravac366@gmail.com>
2026-02-25 16:46:11 +0530

    Add mergeTwoLists function for linked lists
    Implemented a function to merge two sorted linked lists.

--- leetcode2.py
+++ leetcode2.py
@@ -1,1 +1,24 @@
 """ LEETCODE QUESTION 2: Write a code in Python to merge two linked list after sorting them  """
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def mergeTwoLists(self, list1: ListNode, list2: ListNode) -> ListNode:
+        dummy = ListNode()
+        curr = dummy
+        
+        while list1 and list2:
+            if list1.val <= list2.val:
+                curr.next = list1
+                list1 = list1.next
+            else:
+                curr.next = list2
+                list2 = list2.next
+            curr = curr.next
+        
+        curr.next = list1 or list2
+        
+        return dummy.next
